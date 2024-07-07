e0160a889b31aa808485f4096698efea38d8f149
savent <savent_gate@outlook.com>
2024-07-08 01:01:01 +0800

    refactor config header

---
+++ source/inc/spn/config.h
@@ -0,0 +1,17 @@
+#pragma once
+
+/**
+ * @brief Response DCP device options that are supported by the device.
+ *
+ */
+#ifndef SPN_DCP_IDENT_RESP_DEVICE_OPTIONS
+#define SPN_DCP_IDENT_RESP_DEVICE_OPTIONS 1
+#endif
+
+/**
+ * @brief Response DCP device station of name
+ *
+ */
+#ifndef SPN_DCP_IDENT_RESP_STATION_OF_NAME
+#define SPN_DCP_IDENT_RESP_STATION_OF_NAME 1
+#endif
