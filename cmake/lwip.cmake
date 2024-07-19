1c36e64b22538245f23b8ce21c682416b24c62fd
savent <savent_gate@outlook.com>
2024-07-19 12:48:02 +0100

    port for win

--- cmake/lwip.cmake
+++ cmake/lwip.cmake
@@ -4,18 +4,33 @@ set(LWIP_CONTRIB_DIR ${LWIP_DIR}/contrib)
 include(${LWIP_DIR}/contrib/ports/CMakeCommon.cmake)
 
 set (LWIP_DEFINITIONS -DLWIP_DEBUG)
+
+if (${CMAKE_HOST_SYSTEM_NAME} MATCHES "Linux")
+    set (LWIP_PORT_INC 
+        "${LWIP_CONTRIB_DIR}/ports/unix/port/include"
+        "${CMAKE_CURRENT_SOURCE_DIR}/source/port/posix")
+    set (LWIP_PORT_SRC
+        "${lwipcontribportunix_SRCS}"
+        "${lwipcontribportunixnetifs_SRCS}")
+else ()
+    set (LWIP_PORT_INC 
+        "${LWIP_CONTRIB_DIR}/ports/win32/include"
+        "${CMAKE_CURRENT_SOURCE_DIR}/source/port/win32")
+    set (LWIP_PORT_SRC
+        "${lwipcontribportwin32_SRCS}"
+        "${lwipcontribportwin32netifs_SRCS}")
+endif ()
 set (LWIP_INCLUDE_DIRS
     "${LWIP_DIR}/src/include"
     "${LWIP_CONTRIB_DIR}/"
-    "${LWIP_CONTRIB_DIR}/ports/unix/port/include"
-    "${CMAKE_CURRENT_SOURCE_DIR}/source/port/posix"
+    ${LWIP_PORT_INC}
 )
 
 set (LWIP_EXCLUDE_SLIPIF TRUE)
 include(${LWIP_CONTRIB_DIR}/ports/unix/Filelists.cmake)
 include(${LWIP_DIR}/src/Filelists.cmake)
 
-add_library(lwip ${lwipnoapps_SRCS} ${lwipcontribportunix_SRCS} ${lwipcontribportunixnetifs_SRCS} ${lwipsnmp_SRCS})
+add_library(lwip ${lwipnoapps_SRCS} ${lwipsnmp_SRCS} ${LWIP_PORT_SRC})
 target_compile_options(lwip PRIVATE ${LWIP_COMPILER_FLAGS})
 target_compile_definitions(lwip PUBLIC ${LWIP_DEFINITIONS} ${LWIP_MBEDTLS_DEFINITIONS})
 target_include_directories(lwip PUBLIC ${LWIP_INCLUDE_DIRS} ${LWIP_MBEDTLS_INCLUDE_DIRS})
---
+++ cmake/spn.cmake
@@ -0,0 +1,26 @@
+if (${CMAKE_HOST_SYSTEM_NAME} MATCHES "Linux")
+    set(SPN_C_FLAGS ${LWIP_COMPILER_FLAGS}  -Wno-c90-c99-compat -Wno-pedantic)
+else ()
+    set(SPN_C_FLAGS ${CMAKE_C_FLAGS})
+endif()
+
+# recursive search for source files and set to SOURCES
+file(GLOB_RECURSE SOURCES source/src/*.c)
+add_library(SPN ${SOURCES})
+target_compile_options(SPN PRIVATE ${SPN_C_FLAGS})
+target_include_directories(SPN PUBLIC source/inc)
+target_link_libraries(SPN PUBLIC lwip)
+
+if (CMAKE_SIZE_TOOL)
+add_custom_target(size ALL
+    COMMAND ${CMAKE_SIZE_TOOL} $<TARGET_FILE:SPN> -t
+    DEPENDS SPN
+    COMMENT "Print size of SPN"
+)
+endif ()
+
+if (${CMAKE_HOST_SYSTEM_NAME} MATCHES "Windows")
+    set (PTHREAD_LIB )
+else()
+    set (PTHREAD_LIB pthread)
+endif()
