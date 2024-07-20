ff41756197e6945fe4d4d013f1446a7810871c6e
savent404 <savent_gate@outlook.com>
2024-07-20 06:52:45 +0000

    refactor ident.rsp

--- source/src/dcp/dcp_srv_idnet_rsp.c
+++ source/src/dcp/dcp_srv_idnet_rsp.c
@@ -69,6 +69,97 @@ static inline int obj_strcpy(void* dst, struct db_object* obj) {
   return obj->header.len;
 }
 
+static int pack_ident_rsp(struct dcp_ctx* ctx, uint16_t option, uint16_t block_info, struct dcp_block_gen* block) {
+  struct db_object* obj;
+  int res;
+  block->option = option >> 8;
+  block->sub_option = option & 0xFF;
+  switch (option) {
+    case BLOCK_TYPE(DCP_OPTION_IP, DCP_SUB_OPT_IP_PARAM): {
+      uint32_t ip, mask, gw;
+      if ((res = db_get_interface_object(ctx->db, ctx->interface_id, DB_ID_IP_ADDR, &obj)) < 0) {
+        return 0;
+      }
+      ip = obj->data.u32;
+      if ((res = db_get_interface_object(ctx->db, ctx->interface_id, DB_ID_IP_MASK, &obj)) < 0) {
+        return 0;
+      }
+      mask = obj->data.u32;
+      if ((res = db_get_interface_object(ctx->db, ctx->interface_id, DB_ID_IP_GATEWAY, &obj)) < 0) {
+        return 0;
+      }
+      gw = obj->data.u32;
+      *PTR_OFFSET(block->data, 0, uint16_t) = SPN_HTONS(block_info);
+      *PTR_OFFSET(block->data, 2, uint32_t) = SPN_HTONL(ip);
+      *PTR_OFFSET(block->data, 6, uint32_t) = SPN_HTONL(mask);
+      *PTR_OFFSET(block->data, 10, uint32_t) = SPN_HTONL(gw);
+      block->length = 14;
+      break;
+    }
+    case BLOCK_TYPE(DCP_OPTION_DEV_PROP, DCP_SUB_OPT_DEV_PROP_NAME_OF_VENDOR): {
+      if (db_get_interface_object(ctx->db, ctx->interface_id, DB_ID_NAME_OF_VENDOR, &obj) < 0) {
+        return 0;
+      }
+      *PTR_OFFSET(block->data, 0, uint16_t) = 0;
+      obj_strcpy((char*)block->data + 2, obj);
+      block->length = 2 + obj->header.len;
+      break;
+    }
+    case BLOCK_TYPE(DCP_OPTION_DEV_PROP, DCP_SUB_OPT_DEV_PROP_NAME_OF_STATION): {
+      if (db_get_interface_object(ctx->db, ctx->interface_id, DB_ID_NAME_OF_STATION, &obj) < 0) {
+        return 0;
+      }
+      *PTR_OFFSET(block, 0, uint16_t) = 0;
+      obj_strcpy((char*)block->data + 2, obj);
+      block->length = 2 + obj->header.len;
+      break;
+    }
+    case BLOCK_TYPE(DCP_OPTION_DEV_PROP, DCP_SUB_OPT_DEV_PROP_DEVICE_ID): {
+      uint16_t device_id, vendor_id;
+      if (db_get_interface_object(ctx->db, ctx->interface_id, DB_ID_DEVICE_ID, &obj) < 0) {
+        return 0;
+      }
+      device_id = obj->data.u16;
+      if (db_get_interface_object(ctx->db, ctx->interface_id, DB_ID_VENDOR_ID, &obj) < 0) {
+        return 0;
+      }
+      vendor_id = obj->data.u16;
+
+      *PTR_OFFSET(block->data, 0, uint16_t) = 0;
+      *PTR_OFFSET(block->data, 2, uint16_t) = SPN_HTONS(vendor_id);
+      *PTR_OFFSET(block->data, 4, uint16_t) = SPN_HTONS(device_id);
+      block->length = 6;
+      break;
+    }
+    case BLOCK_TYPE(DCP_OPTION_DEV_PROP, DCP_SUB_OPT_DEV_PROP_DEVICE_ROLE): {
+      if (db_get_interface_object(ctx->db, ctx->interface_id, DB_ID_DEVICE_ROLE, &obj) < 0) {
+        return 0;
+      }
+
+      *PTR_OFFSET(block->data, 0, uint16_t) = 0;
+      *PTR_OFFSET(block->data, 2, uint16_t) = SPN_HTONS(obj->data.u16);
+      block->length = 4;
+      break;
+    }
+    case BLOCK_TYPE(DCP_OPTION_DEV_PROP, DCP_SUB_OPT_DEV_PROP_DEVICE_OPTIONS): {
+      unsigned i;
+      *PTR_OFFSET(block->data, 0, uint16_t) = 0;
+
+      for (i = 0; i < ARRAY_SIZE(supported_options); i++) {
+        uint16_t opt = supported_options[i];
+        *PTR_OFFSET(block->data, 2 + i * 2, uint16_t) = SPN_HTONS(opt);
+      }
+      block->length = 2 + ARRAY_SIZE(supported_options) * 2;
+      break;
+    }
+    default:
+      return 0;
+  }
+  res = block->length;
+  block->length = SPN_HTONS((block->length));
+  return sizeof(*block) + ((res + 1) & ~1);
+}
+
 int dcp_srv_ident_rsp(struct dcp_ctx* ctx, struct dcp_mcr_ctx* mcr, void* payload, uint16_t length) {
   struct dcp_header* hdr = (struct dcp_header*)payload;
   struct db_object* obj;
@@ -85,98 +176,18 @@ int dcp_srv_ident_rsp(struct dcp_ctx* ctx, struct dcp_mcr_ctx* mcr, void* payloa
     uint16_t option = mandatory_options[idx];
     struct dcp_block_gen* block = (struct dcp_block_gen*)((uintptr_t)payload + offset);
 
-    block->option = option >> 8;
-    block->sub_option = option & 0xFF;
-
-    switch (option) {
-      case BLOCK_TYPE(DCP_OPTION_IP, DCP_SUB_OPT_IP_PARAM): {
-        uint32_t ip, mask, gw;
-        if ((res = db_get_interface_object(ctx->db, ctx->interface_id, DB_ID_IP_ADDR, &obj)) < 0) {
-          goto invalid_option;
-        }
-        ip = obj->data.u32;
-        if ((res = db_get_interface_object(ctx->db, ctx->interface_id, DB_ID_IP_MASK, &obj)) < 0) {
-          goto invalid_option;
-        }
-        mask = obj->data.u32;
-        if ((res = db_get_interface_object(ctx->db, ctx->interface_id, DB_ID_IP_GATEWAY, &obj)) < 0) {
-          goto invalid_option;
-        }
-        gw = obj->data.u32;
-        *PTR_OFFSET(block->data, 0, uint16_t) = SPN_HTONS(block_info);
-        *PTR_OFFSET(block->data, 2, uint32_t) = SPN_HTONL(ip);
-        *PTR_OFFSET(block->data, 6, uint32_t) = SPN_HTONL(mask);
-        *PTR_OFFSET(block->data, 10, uint32_t) = SPN_HTONL(gw);
-        block->length = 14;
-        break;
-      }
-      case BLOCK_TYPE(DCP_OPTION_DEV_PROP, DCP_SUB_OPT_DEV_PROP_NAME_OF_VENDOR): {
-        if (db_get_interface_object(ctx->db, ctx->interface_id, DB_ID_NAME_OF_VENDOR, &obj) < 0) {
-          goto invalid_option;
-        }
-        *PTR_OFFSET(block->data, 0, uint16_t) = 0;
-        obj_strcpy((char*)block->data + 2, obj);
-        block->length = 2 + obj->header.len;
-        break;
-      }
-      case BLOCK_TYPE(DCP_OPTION_DEV_PROP, DCP_SUB_OPT_DEV_PROP_NAME_OF_STATION): {
-        if (db_get_interface_object(ctx->db, ctx->interface_id, DB_ID_NAME_OF_STATION, &obj) < 0) {
-          goto invalid_option;
-        }
-        *PTR_OFFSET(block, 0, uint16_t) = 0;
-        obj_strcpy((char*)block->data + 2, obj);
-        block->length = 2 + obj->header.len;
-        break;
-      }
-      case BLOCK_TYPE(DCP_OPTION_DEV_PROP, DCP_SUB_OPT_DEV_PROP_DEVICE_ID): {
-        uint16_t device_id, vendor_id;
-        if (db_get_interface_object(ctx->db, ctx->interface_id, DB_ID_DEVICE_ID, &obj) < 0) {
-          goto invalid_option;
-        }
-        device_id = obj->data.u16;
-        if (db_get_interface_object(ctx->db, ctx->interface_id, DB_ID_VENDOR_ID, &obj) < 0) {
-          goto invalid_option;
-        }
-        vendor_id = obj->data.u16;
-
-        *PTR_OFFSET(block->data, 0, uint16_t) = 0;
-        *PTR_OFFSET(block->data, 2, uint16_t) = SPN_HTONS(vendor_id);
-        *PTR_OFFSET(block->data, 4, uint16_t) = SPN_HTONS(device_id);
-        block->length = 6;
-        break;
-      }
-      case BLOCK_TYPE(DCP_OPTION_DEV_PROP, DCP_SUB_OPT_DEV_PROP_DEVICE_ROLE): {
-        if (db_get_interface_object(ctx->db, ctx->interface_id, DB_ID_DEVICE_ROLE, &obj) < 0) {
-          goto invalid_option;
-        }
-
-        *PTR_OFFSET(block->data, 0, uint16_t) = 0;
-        *PTR_OFFSET(block->data, 2, uint16_t) = SPN_HTONS(obj->data.u16);
-        block->length = 4;
-        break;
-      }
-      case BLOCK_TYPE(DCP_OPTION_DEV_PROP, DCP_SUB_OPT_DEV_PROP_DEVICE_OPTIONS): {
-        unsigned i;
-        *PTR_OFFSET(block->data, 0, uint16_t) = 0;
-
-        for (i = 0; i < ARRAY_SIZE(supported_options); i++) {
-          uint16_t opt = supported_options[i];
-          *PTR_OFFSET(block->data, 2 + i * 2, uint16_t) = SPN_HTONS(opt);
-        }
-        block->length = 2 + ARRAY_SIZE(supported_options) * 2;
-        break;
-      }
-      default:
-        goto invalid_option;
+    res = pack_ident_rsp(ctx, option, block_info, block);
+    if (res == 0) {
+      continue;
     }
 
-    offset = (sizeof(*block) + block->length + 1) & ~1;
-    block->length = SPN_HTONS(block->length);
+    offset += res;
 
     if (offset > length) {
       return -SPN_EMSGSIZE;
     }
   }
+
   SPN_ASSERT("So tiny!", offset <= SPN_RTC_MAXIMAL_FRAME_SIZE);
   hdr->service_id = DCP_SRV_ID_IDENT;
   hdr->service_id = DCP_SRV_TYPE_RES;
