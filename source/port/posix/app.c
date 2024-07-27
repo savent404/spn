222ded0e870591177c85e163b9d799885be643a3
Savent Gate <savent_gate@outlook.com>
2024-07-27 10:43:09 +0800

    app: dump device info

--- source/port/posix/app.c
+++ source/port/posix/app.c
@@ -1,8 +1,13 @@
 #include <assert.h>
+#include <lwip/debug.h>
+#include <lwip/ip.h>
+#include <lwip/ip_addr.h>
 #include <lwip/timeouts.h>
+#include <spn/db.h>
 #include <spn/dcp.h>
 #include <spn/spn.h>
 #include <stdio.h>
+#include <string.h>
 
 enum app_state {
   APP_STATE_INIT,
@@ -18,7 +23,7 @@ struct app_instance {
 };
 
 static void app_rta_timer_handler(void* arg) {
-  struct app_instance* inst = &app_inst;
+  struct app_instance* inst = (struct app_instance*)arg;
   unsigned next_time = 1000;
   printf("rta timer handler\n");
 
@@ -34,14 +39,79 @@ static void app_rta_timer_handler(void* arg) {
         pbuf_free(p);
       }
       next_time = 500;  // device should response in 400ms, but we need some mercy time
-      APP_STATE_DCP;
       break;
     case APP_STATE_DCP:
       printf("dcp state\n");
-
       // make sure that ident.req context is cleared, and device should be located in the db
-      assert(inst->ctx->dcp.mcs_ctx.resp_options_bitmap == 0);
+      assert(inst->ctx->dcp.mcs_ctx.req_options_bitmap == 0);
       assert(inst->ctx->dcp.mcs_ctx.state == DCP_STATE_IDLE);
+      assert(inst->ctx->dcp.mcs_ctx.xid == 0x88000001);
+      assert(inst->ctx->dcp.mcs_ctx.response_interface_id == SPN_EXTERNAL_INTERFACE_BASE + 1);
+      {
+        struct db_interface* iface;
+        struct db_object* obj;
+        char* station_name;
+        int res;
+
+        res = db_get_interface(&inst->ctx->db, SPN_EXTERNAL_INTERFACE_BASE + 1, &iface);
+        assert(res == SPN_OK);
+
+        res = db_get_object(&iface->objects, DB_ID_NAME_OF_INTERFACE, &obj);
+        assert(res == SPN_OK);
+
+        station_name = (char*)malloc(obj->attr.len + 1);
+        if (obj->attr.is_dyn) {
+          memcpy(station_name, obj->data.ptr, obj->attr.len);
+          station_name[obj->attr.len] = '\0';
+        } else {
+          memcpy(station_name, obj->data.str, obj->attr.len);
+          station_name[obj->attr.len] = '\0';
+        }
+        printf("station name: %s\n", station_name);
+        free(station_name);
+
+        res = db_get_object(&iface->objects, DB_ID_VENDOR_ID, &obj);
+        if (res != SPN_OK) {
+          printf("vendor id not found\n");
+        } else {
+          printf("vendor id: %04x\n", obj->data.u16);
+        }
+
+        res = db_get_object(&iface->objects, DB_ID_DEVICE_ID, &obj);
+        if (res != SPN_OK) {
+          printf("device id not found\n");
+        } else {
+          printf("device id: %04x\n", obj->data.u16);
+        }
+
+        res = db_get_object(&iface->objects, DB_ID_DEVICE_ROLE, &obj);
+        if (res != SPN_OK) {
+          printf("device role not found\n");
+        } else {
+          printf("device role: %02x\n", obj->data.u8);
+        }
+
+        res = db_get_object(&iface->objects, DB_ID_IP_ADDR, &obj);
+        if (res != SPN_OK) {
+          printf("ip address not found\n");
+        } else {
+          printf("device ip: %s\n", ip4addr_ntoa((ip4_addr_t*)&obj->data.u32));
+        }
+
+        res = db_get_object(&iface->objects, DB_ID_IP_MASK, &obj);
+        if (res != SPN_OK) {
+          printf("subnet mask not found\n");
+        } else {
+          printf("subnet mask: %s\n", ip4addr_ntoa((ip4_addr_t*)&obj->data.u32));
+        }
+
+        res = db_get_object(&iface->objects, DB_ID_IP_GATEWAY, &obj);
+        if (res != SPN_OK) {
+          printf("gateway not found\n");
+        } else {
+          printf("gateway: %s\n", ip4addr_ntoa((ip4_addr_t*)&obj->data.u32));
+        }
+      }
       break;
     case APP_STATE_PRM:
       printf("prm state\n");
