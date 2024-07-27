f56c6e8ba17a57ca301388dc8b05ca73a7abfabc
Savent Gate <savent_gate@outlook.com>
2024-07-27 15:44:46 +0800

    chore: Add call to generic ethernet_output in dcp_output.c

--- source/src/dcp/dcp_output.c
+++ source/src/dcp/dcp_output.c
@@ -7,6 +7,7 @@ __attribute__((weak)) int dcp_output(struct dcp_ctx* ctx,
                                      const struct eth_addr* dst,
                                      struct pbuf* p) {
   SPN_UNUSED_ARG(ctx);
+  /* TODO: should call generic ethernet_output of spn */
   const struct eth_addr* src = (const struct eth_addr*)&iface->netif.hwaddr;
   ethernet_output(&iface->netif, p, src, dst, ETHTYPE_PROFINET);
   return 0;
