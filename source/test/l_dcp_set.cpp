a8cc0486d0b8a2bee64965aba4b7e2c3e524e7e0
Savent Gate <savent_gate@outlook.com>
2024-07-30 00:32:06 +0800

    improved set.req's code covrage rate

--- source/test/l_dcp_set.cpp
+++ source/test/l_dcp_set.cpp
@@ -81,6 +81,22 @@ TEST_F(DcpSet, SetNameOfStation_odd) {
   EXPECT_EQ(db_is_static_string_object(obj), true);
   EXPECT_EQ(strncmp(obj->data.str, "fooboo1", 7), 0);
 }
+TEST_F(DcpSet, SetNameOfStation_timeout) {
+  static char buf[1500];
+  uint16_t buf_len;
+  struct dcp_ucr_ctx ucr;
+
+  tain_buffer(buf, sizeof(buf));
+  controller->dcp.ucs_ctx.req_options_bitmap = 1 << DCP_BIT_IDX_DEV_PROP_NAME_OF_STATION;
+  controller->dcp.ucs_ctx.station_name = "fooboo";
+  ASSERT_EQ(dcp_srv_set_req(&controller->dcp, &controller->dcp.ucs_ctx, buf, &buf_len), SPN_OK);
+  ASSERT_EQ(dcp_srv_set_ind(&device->dcp, &ucr, buf + 2, buf_len - 2), SPN_OK);
+  tain_buffer(buf, sizeof(buf));
+  ASSERT_EQ(dcp_srv_set_rsp(&controller->dcp, &ucr, buf, &buf_len), SPN_OK);
+  _dcp_srv_set_req_timeout(&controller->dcp.ucs_ctx);
+  ASSERT_EQ(dcp_srv_set_cnf(&device->dcp, &controller->dcp.ucs_ctx, buf + 2, buf_len - 2), -SPN_EAGAIN);
+}
+
 
 TEST_F(DcpSet, SetNameOfStation_uppercase) {
   static char buf[1500];
