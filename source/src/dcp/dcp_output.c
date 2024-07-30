3e741e81babaa914dd005e3c3e39e67dc06bb00e
Savent Gate <savent_gate@outlook.com>
2024-07-30 22:40:21 +0800

    chore: Refactor dcp_output function for improved code clarity and performance

--- source/src/dcp/dcp_output.c
+++ source/src/dcp/dcp_output.c
@@ -8,7 +8,6 @@ __attribute__((weak)) int dcp_output(struct dcp_ctx* ctx,
                                      spn_frame_t f) {
   SPN_UNUSED_ARG(ctx);
   /* TODO: should call generic ethernet_output of spn */
-  const struct eth_addr* src = (const struct eth_addr*)&iface->netif.hwaddr;
   spn_send_frame(iface, f, (const uint8_t*)dst);
   return 0;
 }
