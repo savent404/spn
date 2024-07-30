54c80c48f661e49807291a9980424c29342eff9c
Savent Gate <savent_gate@outlook.com>
2024-07-30 23:35:26 +0800

    chore: Remove unused code and update external interface ID in dcp_init

--- source/src/dcp/dcp_init.c
+++ source/src/dcp/dcp_init.c
@@ -11,4 +11,5 @@ void dcp_init(struct dcp_ctx* ctx, struct db_ctx* db) {
   ctx->mcs_ctx.xid = 0x88000000;
   ctx->mcs_ctx.response_delay_factory = 1;
   ctx->mcs_ctx.external_interface_id = SPN_EXTERNAL_INTERFACE_BASE;
+  ctx->ucs_ctx.ex_ifr = SPN_EXTERNAL_INTERFACE_BASE;
 }
