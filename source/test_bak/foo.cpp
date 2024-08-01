488f7fc318afff389fa0cad828ad1458bb760f01
savent <savent_gate@outlook.com>
2024-08-01 03:21:40 +0100

    remove lagecy codes

--- source/test_bak/foo.cpp
+++
@@ -1,5 +0,0 @@
-#include <gtest/gtest.h>
-
-TEST(FooTest, Test1) {
-  EXPECT_EQ(1, 1);
-}
--- source/test_bak/global.cpp
+++
@@ -1,4 +0,0 @@
-#include "global.hpp"
-
-LwipCtx* LwipCtx::instance = nullptr;
-LwipCtx::level LwipCtx::init_lvl = LwipCtx::level::none;
